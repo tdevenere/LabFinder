25e041b7fb8621092cfe3a896cd1057408c6fe13
CONDOR <connorjhopkins@gmail.com>
2019-02-16 15:09:52 -0800

    fixes tiny navigation error

--- app/navigation/Routes.js
+++ app/navigation/Routes.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-import { createDrawerNavigator, StackNavigator } from 'react-navigation';
+import { createDrawerNavigator, StackNavigator, createAppContainer } from 'react-navigation';
 
 import StackNavigation from './StackNavigation';
 import SideMenu from '../screens/SideMenu';
 
 
-export default createDrawerNavigator({
+const stack = createDrawerNavigator({
     Options: { screen: StackNavigation },
 },
 {
@@ -14,3 +14,5 @@ export default createDrawerNavigator({
     drawerWidth: 300,
     headerMode: 'none',
 });
+
+export default createAppContainer(stack);
