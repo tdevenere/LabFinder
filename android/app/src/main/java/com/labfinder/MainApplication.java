d6e941951056046a6e2c25c156e502a61df9b80c
Owen <35277141+wabajack12@users.noreply.github.com>
2019-02-16 21:16:18 -0800

    Clean copy

--- android/app/src/main/java/com/labfinder/MainApplication.java
+++ android/app/src/main/java/com/labfinder/MainApplication.java
@@ -3,9 +3,6 @@ package com.labfinder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.mapbox.rctmgl.RCTMGLPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new RCTMGLPackage(),
-          new RNGestureHandlerPackage(),
-          new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
 
